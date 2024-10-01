94020b12b2c1cbfddc433bb267a8772c6f859a86
anshul.tiwari <anshulansh2016@gmail.com>
2024-10-01 15:14:09 +0530

    1st program in RestAPI

--- src/test/java/com/testAcademy/Test002.java
+++ src/test/java/com/testAcademy/Test002.java
@@ -1,7 +1,23 @@
 package com.testAcademy;
 
+import io.restassured.RestAssured;
+
 public class Test002 {
     public static void main(String[] args) {
+        System.out.println("Rest Assured test cases");
+        System.out.println("Demo");
+
+        //This is the URL : https://restful-booker.herokuapp.com/booking/1
+        //log all is see the all actions
+        RestAssured
+                .given()
+                    .baseUri("https://restful-booker.herokuapp.com/")
+                    .basePath("/booking/1").log().all()
+                .when()
+                    .get()
+                .then().log().all()
+                    .statusCode(200);
+
 
     }
 }
